a284ad1d73f0f7991f562d0cd3d3172e7e0603c6
Devang Chaudhari <devangchaudhari2003@gmail.com>
2024-04-04 20:24:16 +0530

    refresh

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,13 @@ app.use(cors(
 ));
 app.use(express.json());
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 //Endpoint for certificate question
 app.get('/certify', (req, res) => {
   res.json(certify);
