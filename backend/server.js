67c86063b16c8d195abb80a9ff9a2424480faf70
Devang Chaudhari <devangchaudhari2003@gmail.com>
2024-04-04 23:38:10 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 
@@ -14,25 +13,29 @@ const questions = {
   tcs: require('./tcs.json'),
   infosys: require('./infosys.json'),
   accenture: require('./accenture.json'),
+  
   // Add more categories if needed
 };
 const certify = require('./certificate.json');
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "https://apti-q.vercel.app", // Update with your Vercel app URL
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(cors(
+  {
+    origin: ["https://apti-q.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 
-// Serve static files from the AptiQ/dist directory
-app.use(express.static(path.join(__dirname, '..', 'AptiQ', 'dist')));
 
-// Endpoint for certificate question
+
+
+//Endpoint for certificate question
 app.get('/certify', (req, res) => {
   res.json(certify);
 });
+
 
 // Endpoint to get questions based on category
 app.get('/questions/:category', (req, res) => {
@@ -66,12 +69,11 @@ app.post('/api/submit-answers', (req, res) => {
   res.json(feedback);
 });
 
-// Serve index.html for any route (necessary for client-side routing)
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'AptiQ', 'dist', 'index.html'));
-});
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
 
 
 
