c4d7c8d181eca7112f578a5ab894044a9b4eb9fb
Devang Chaudhari <devangchaudhari2003@gmail.com>
2024-04-04 16:40:47 +0530

    server

--- AptiQ/src/components/Certification.jsx
+++ AptiQ/src/components/Certification.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-// import Certify from './Certify';
+
 
 const Certification = () => {
   const [questions, setQuestions] = useState([]);
